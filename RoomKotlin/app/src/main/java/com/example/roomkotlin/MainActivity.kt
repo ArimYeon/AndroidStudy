e2cd9079044e20ec666a735fb27175f81f982a8e
arimyeon <dusdkfla@naver.com>
2021-01-01 14:11:27 +0900

    LiveData study

--- RoomKotlin/app/src/main/java/com/example/roomkotlin/MainActivity.kt
+++ RoomKotlin/app/src/main/java/com/example/roomkotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.roomkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,11 +14,13 @@ class MainActivity : AppCompatActivity() {
         val db = Room.databaseBuilder(this, AppDatabase::class.java, "todo-db")
                 .allowMainThreadQueries().build()
 
-        result_text.text = db.todoDao().getAll().toString()
+        // LiveData
+        db.todoDao().getAll().observe(this, Observer {
+            result_text.text = it.toString()
+        })
 
         add_button.setOnClickListener {
             db.todoDao().insert(Todo(todo_edit.text.toString()))
-            result_text.text = db.todoDao().getAll().toString()
         }
     }
 }
